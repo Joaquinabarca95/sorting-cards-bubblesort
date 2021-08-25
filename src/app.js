318674794339a968b821039b83687d58ac98c13e
Joaquinabarca95 <joaquinabarca@live.com>
2021-08-25 17:18:07 +0000

    draw function complete

--- src/app.js
+++ src/app.js
@@ -8,18 +8,33 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  let drawButton = document.querySelector(".draw");
+  let drawButton = document.querySelector(".drawBtn");
   drawButton.addEventListener("click", drawRandomCards);
+  let sortButton = document.querySelector(".sortBtn");
+  sortButton.addEventListener("click", bubbleSortCards);
 
-  let amount = document.querySelector(".amount");
-
+  let inputAmount = document.querySelector(".amount");
+  let cardAmount = [];
   function drawRandomCards() {
-    let cardDiv = document.createElement("div");
-    cardDiv.classList.add("card", "number");
-    let parent = document.querySelector(".parent");
-    parent.append(cardDiv);
-    document.querySelector(".card").classList.add(randomSuitGenerator());
-    document.querySelector(".number").innerHTML = randomNumberGenerator();
+    let amount = parseInt(inputAmount.value);
+    // console.log(amount);
+
+    for (let i = 0; i < amount; i++) {
+      let newCard = document.createElement("div");
+      newCard.classList.add("card", "number", randomSuitGenerator());
+      newCard.setAttribute("style", "display:flex;");
+      newCard.innerHTML = randomNumberGenerator();
+      let parent = document.querySelector(".cardsDiv");
+      parent.append(newCard);
+      cardAmount.push(newCard);
+      // console.log(cardAmount);
+      if (newCard != undefined) {
+        function disableBtn() {
+          document.getElementById("draw").disabled = true;
+        }
+        disableBtn();
+      }
+    }
 
     function randomNumberGenerator() {
       let numbers = [
@@ -46,5 +61,25 @@ window.onload = function() {
       let randomSuits = Math.floor(Math.random() * suits.length);
       return suits[randomSuits];
     }
+  }
+
+  function bubbleSortCards(arr) {
+    console.log(cardAmount);
+    arr = cardAmount;
+    let wall = arr.length - 1;
+    while (wall > 0) {
+      let index = 0;
+      while (index < wall) {
+        if (arr[index] > arr[index + 1]) {
+          let aux = arr[index];
+          arr[index] = arr[index + 1];
+          arr[index + 1] = aux;
+        }
+        index++;
+      }
+      wall--;
+    }
+    console.log(arr);
+    return arr;
   }
 };
